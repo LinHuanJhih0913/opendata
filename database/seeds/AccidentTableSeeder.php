bedd282a836c50848e445922277b94e022fb3d39
Jett <newjett0617@gmail.com>
2018-11-08 14:08:01 +0800

    [Modify]accident data set, update accident seed

--- database/seeds/AccidentTableSeeder.php
+++ database/seeds/AccidentTableSeeder.php
@@ -12,15 +12,15 @@ class AccidentTableSeeder extends Seeder
     public function run()
     {
         // GPS data
-        if (($handle = fopen(public_path("raw/accident-20180101-20180906.csv"), "r")) !== FALSE) {
+        if (($handle = fopen(public_path("raw/accident-20180101-20180831.csv"), "r")) !== FALSE) {
             while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
                 \App\Accident::create([
                     'date' => $data[0],
                     'time' => $data[1],
                     'city' => $data[2],
                     'district' => $data[3],
-                    'gps_longitude' => $data[6],
-                    'gps_latitude' => $data[7],
+                    'gps_longitude' => $data[4],
+                    'gps_latitude' => $data[5],
                 ]);
             }
             fclose($handle);
