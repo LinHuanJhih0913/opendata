5510462519fa97a7575f90224ba0de72e658c02d
Jett <newjett0617@gmail.com>
2018-11-08 11:26:33 +0800

    [Modify]RainController use rainfall model

--- app/Http/Controllers/RainController.php
+++ app/Http/Controllers/RainController.php
@@ -2,7 +2,8 @@
 
 namespace App\Http\Controllers;
 
-use App\Rain;
+
+use App\Rainfall;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
@@ -17,9 +18,9 @@ class RainController extends Controller
                 'kindof' => $queryString->get('kindof'),
                 'year' => $queryString->get('year'),
                 'month' => $queryString->get('month'),
-                'items' => Rain::select(['month', DB::raw('SUM(rain_rate) total')])
-                    ->where('year', $queryString->get('year'))
-                    ->groupBy('month')
+                'items' => Rainfall::select([DB::raw('MONTH(date) as month, SUM(rainfall) as rain_rate')])
+                    ->where(DB::raw('YEAR(date)'), $queryString->get('year'))
+                    ->groupBy(DB::raw('MONTH(date)'))
                     ->get(),
             ], 200, [
                 'Access-Control-Allow-Origin' => '*',
@@ -31,9 +32,9 @@ class RainController extends Controller
                 'kindof' => $queryString->get('kindof'),
                 'year' => $queryString->get('year'),
                 'month' => $queryString->get('month'),
-                'items' => Rain::select(['day', 'rain_rate'])
-                    ->where('year', $queryString->get('year'))
-                    ->where('month', $queryString->get('month'))
+                'items' => Rainfall::select([DB::raw('day(date) as day, rainfall as rain_rate')])
+                    ->where(DB::raw('YEAR(date)'), $queryString->get('year'))
+                    ->where(DB::raw('MONTH(date)'), $queryString->get('month'))
                     ->get(),
             ], 200, [
                 'Access-Control-Allow-Origin' => '*',
